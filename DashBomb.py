2fc19664e778fd56270a77585c2b4f9ebe240bd1
Arda Özarslan <ozarslanarda55@gmail.com>
2020-10-22 20:03:15 +0300

    .

--- DashBomb.py
+++ DashBomb.py
@@ -6,6 +6,7 @@ height = 900
 screen = pygame.display.set_mode((width, height))
 
 player = pygame.image.load("player.png")
+tiles = pygame.image.load("tiles.png")
 
 while True:
     screen.fill(0)
@@ -13,6 +14,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    for x in range(int(width / tiles.get_width())):
+        screen.blit(tiles, (100 * x, 300))
 
     screen.blit(player, [500, 400])
 
