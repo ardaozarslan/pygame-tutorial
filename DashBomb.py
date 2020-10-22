a5dd157665fce1eea0475b0c8d42a2c240a3cc8e
Arda Özarslan <ozarslanarda55@gmail.com>
2020-10-22 19:54:22 +0300

    .

--- DashBomb.py
+++ DashBomb.py
@@ -5,11 +5,15 @@ width = 1600
 height = 900
 screen = pygame.display.set_mode((width, height))
 
+player = pygame.image.load("player.png")
+
 while True:
     screen.fill(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(player, [500, 400])
 
     pygame.display.flip()
