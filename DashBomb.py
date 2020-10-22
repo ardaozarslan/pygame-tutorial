0b60b9fe167b0c283b4c6cb4ac9b6c956f7e8e33
Arda Özarslan <ozarslanarda55@gmail.com>
2020-10-22 19:40:07 +0300

    .

--- DashBomb.py
+++ DashBomb.py
@@ -4,3 +4,12 @@ pygame.init()
 width = 1600
 height = 900
 screen = pygame.display.set_mode((width, height))
+
+while True:
+    screen.fill(0)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.flip()
